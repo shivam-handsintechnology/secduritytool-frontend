deb278706b99a5296d9703c5c4c87bd2404cacb5
shivam-handsintechnology <shivam@handsintechnology.com>
2024-05-02 19:24:08 +0530

    fadf

--- src/components/Protection_modules/SecurityMisconfiguration.jsx
+++ src/components/Protection_modules/SecurityMisconfiguration.jsx
@@ -9,13 +9,13 @@ import LoadingSpinner from '../LoaderAndError/loader';
 
 const SecurityMisconfiguration = (props) => {
     const userData = useSelector((state) => state.UserReducer)
-    const arbitraryMethods = useDataFetch(`SecurityMisconfiguration/arbitrary-methods?domain=${userData.domain}`, [userData.domain],[])
-    const dangerousMethods = useDataFetch(`SecurityMisconfiguration/dangerous-http-methods-enabled?domain=${userData.domain}`, [userData.domain],[])
-    const passwordTestHash = useDataFetch(`SecurityMisconfiguration/passwords-insecure?domain=${userData.domain}`, [userData.domain],"")
-    const passwordWeekTest = useDataFetch(`SecurityMisconfiguration/week-passwords-insecure?domain=${userData.domain}`, [userData.domain],"")
-    const Nodejs = useDataFetch(`SecurityMisconfiguration/support-oldnodejs-version?domain=${userData.domain}`, [userData.domain])    
-    const OptionMethods = useDataFetch(`SecurityMisconfiguration/option-methods-enabled?domain=${userData.domain}`, [userData.domain],[])    
-  
+    const arbitraryMethods = useDataFetch(`SecurityMisconfiguration/arbitrary-methods?domain=${userData.domain}`, [userData.domain], null)
+    const dangerousMethods = useDataFetch(`SecurityMisconfiguration/dangerous-http-methods-enabled?domain=${userData.domain}`, [userData.domain], [])
+    const passwordTestHash = useDataFetch(`SecurityMisconfiguration/passwords-insecure?domain=${userData.domain}`, [userData.domain], "")
+    const passwordWeekTest = useDataFetch(`SecurityMisconfiguration/week-passwords-insecure?domain=${userData.domain}`, [userData.domain], "")
+    const Nodejs = useDataFetch(`SecurityMisconfiguration/support-oldnodejs-version?domain=${userData.domain}`, [userData.domain])
+    const OptionMethods = useDataFetch(`SecurityMisconfiguration/option-methods-enabled?domain=${userData.domain}`, [userData.domain], [])
+
     return (
         <React.Fragment>
             <div className="card card-primary card-outline">
@@ -23,21 +23,22 @@ const SecurityMisconfiguration = (props) => {
                     <h5><i className="fas fa-shield-alt" /> &nbsp;Protection Modules (Security Misconfiguration)</h5>
                 </div>
                 <div className="card-body">
-                   
-                            <ul>
-                                <li className='list-unstyled'>Application accepts arbitrary methods :{
-                                    arbitraryMethods.errors.loading?<LoadingSpinner/>:arbitraryMethods.errors.error?<span className=' error'>{arbitraryMethods.errors.message}</span>:
-                                arbitraryMethods.data && arbitraryMethods.data.length>0?"Yes":"No"}</li>
-                                
-                                <li  className='list-unstyled'>Application database stores password in plain text :  {passwordTestHash.errors.loading?<LoadingSpinner/>:passwordTestHash.errors.error?<span className=' error'>{passwordTestHash.errors.message}</span>:passwordTestHash.data}</li>
-                                <li  className='list-unstyled'>Support Old Nodejs Version :{Nodejs.errors.loading?<LoadingSpinner/>:Nodejs.errors.error?<span className=' error'>{Nodejs.errors.message}</span>:Nodejs.data && Nodejs.data.older_version_support ? "Yes" : "No"}</li>
-                                <li  className='list-unstyled'>Dangerous HTTP Methods : {dangerousMethods.errors.loading?<LoadingSpinner/>:dangerousMethods.errors.error?<span className=' error'>{dangerousMethods.errors.message}</span>:dangerousMethods.data && dangerousMethods.data.length > 0 ? "Yes":"No"}</li>
-                                <li  className='list-unstyled'>Option Methods Enabled : {OptionMethods.errors.loading?<LoadingSpinner/>:OptionMethods.errors.error?<span className=' error'>{OptionMethods.errors.message}</span>:OptionMethods.data && OptionMethods.data.length>0?"Yes":"No"}</li>
-                                <li  className='list-unstyled'>Password is encrypted/encoded using weak algorithm : {passwordWeekTest.errors.loading?<LoadingSpinner/>:passwordWeekTest.errors.error?<span className=' error'>{passwordWeekTest.errors.message}</span>:passwordWeekTest.data}</li>
-                             
-                            </ul>
-                        
-                    
+
+                    <ul>
+                        <li className='list-unstyled'>Application accepts arbitrary methods :{
+                            arbitraryMethods.errors.loading ? <LoadingSpinner /> : arbitraryMethods.errors.error ? <span className=' error'>{arbitraryMethods.errors.message}</span> :
+                                arbitraryMethods.data && !arbitraryMethods.data.isAccessControlAllowMethods ?  <span className='error'>Access Control Allow Methods Are Not Enable</span> : 
+                                arbitraryMethods.data && arbitraryMethods.data.results &&    arbitraryMethods.data.results.length > 0 ? "Yes" : "No"}</li>
+
+                        <li className='list-unstyled'>Application database stores password in plain text :  {passwordTestHash.errors.loading ? <LoadingSpinner /> : passwordTestHash.errors.error ? <span className=' error'>{passwordTestHash.errors.message}</span> : passwordTestHash.data}</li>
+                        <li className='list-unstyled'>Support Old Nodejs Version :{Nodejs.errors.loading ? <LoadingSpinner /> : Nodejs.errors.error ? <span className=' error'>{Nodejs.errors.message}</span> : Nodejs.data && Nodejs.data.older_version_support ? "Yes" : "No"}</li>
+                        <li className='list-unstyled'>Dangerous HTTP Methods : {dangerousMethods.errors.loading ? <LoadingSpinner /> : dangerousMethods.errors.error ? <span className=' error'>{dangerousMethods.errors.message}</span> : dangerousMethods.data && dangerousMethods.data.length > 0 ? "Yes" : "No"}</li>
+                        <li className='list-unstyled'>Option Methods Enabled : {OptionMethods.errors.loading ? <LoadingSpinner /> : OptionMethods.errors.error ? <span className=' error'>{OptionMethods.errors.message}</span> : OptionMethods.data && OptionMethods.data.length > 0 ? "Yes" : "No"}</li>
+                        <li className='list-unstyled'>Password is encrypted/encoded using weak algorithm : {passwordWeekTest.errors.loading ? <LoadingSpinner /> : passwordWeekTest.errors.error ? <span className=' error'>{passwordWeekTest.errors.message}</span> : passwordWeekTest.data}</li>
+
+                    </ul>
+
+
                     <ul>
                     </ul>
                 </div>
--- src/pages/Security_Misconfiguration/Arbitrarymethods.jsx
+++ src/pages/Security_Misconfiguration/Arbitrarymethods.jsx
@@ -2,44 +2,50 @@ import React from 'react'
 import { useDataFetch, usePostData } from '../../hooks/DataFetchHook'
 import { useSelector } from 'react-redux'
 import LoadingSpinner from '../../components/LoaderAndError/loader'
-const Arbitrarymethods = () => {
-  const UserData = useSelector((state) => state.UserReducer)
 
-  const postSessionData=useDataFetch(`SecurityMisconfiguration/arbitrary-methods?domain=${UserData.domain}`,[UserData.domain])
-    console.log("postSessionData", postSessionData)
+
+const ArbitraryMethods = () => {
+  const UserData = useSelector((state) => state.UserReducer);
+  
+  const postSessionData = useDataFetch(`SecurityMisconfiguration/arbitrary-methods?domain=${UserData.domain}`, [UserData.domain]);
+  console.log("postSessionData", postSessionData);
+
   return (
     <div>
-    {postSessionData.errors.loading ? <LoadingSpinner /> : postSessionData.errors.error ? <span className='error'>{postSessionData.errors.message}</span> :
-     <table className="table table-striped">
-     <tbody>
-       {postSessionData.data && postSessionData.data.length > 0 ? (
-        <>
-        <tr>
-         Arbitary  Methods Enabled
-        </tr>
-        {
-           postSessionData.data.map((obj, index) => (
-            <tr key={index}>
-              {Object.entries(obj).map(([key, value]) => (
-                <React.Fragment key={key}>
-                  <td><strong className="text-capitalize">{key}</strong></td>
-                  <td>{value}</td>
-                </React.Fragment>
-              ))}
-            </tr>
-          ))
-        }
-        </>
-       ) : (
-         <tr>
-           <td colSpan="2">Arbitary Methods Not Found</td>
-         </tr>
-       )}
-     </tbody>
-   </table>
-    }
-  </div>
-  )
-}
+      {postSessionData.errors.loading ? (
+        <LoadingSpinner />
+      ) : postSessionData.errors.error ? (
+        <span className='error'>{postSessionData.errors.message}</span>
+      ) : 
+      postSessionData.data && postSessionData.data.isAccessControlAllowMethods?
+      (
+        <table className="table table-striped">
+          <tbody>
+            {postSessionData.data && postSessionData.data.results && postSessionData.data.results.length > 0 ? (
+              <>
+                <tr>
+                  <td colSpan="2">Arbitrary Methods Enabled</td>
+                </tr>
+                {postSessionData.data.results.map((result, index) => (
+                  <tr key={index}>
+                    <td><strong className="text-capitalize">Results</strong></td>
+                    <td>{result}</td>
+                  </tr>
+                ))}
+              </>
+            ) : (
+              <tr>
+                <td colSpan="2">Arbitrary Methods Not Found</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      ):
+      (
+        <span className='error'>Access Control Allow Methods Are Not Enable</span>
+      )}
+    </div>
+  );
+};
 
-export default Arbitrarymethods
+export default ArbitraryMethods;
